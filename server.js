f6cc98607da978b9b769c664c334e33a85d69579
Agatsya2709 <136240238+Agatsya2709@users.noreply.github.com>
2026-07-24 00:49:11 +0530

    commit

--- server.js
+++ server.js
@@ -1,26 +1,38 @@
-import http from 'http';
-import path from 'node:path';
-import fs from 'fs/promises';
-const port = 8000;
+import http from "node:http";
+import path from "node:path";
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+
+const port = 8080;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = http.createServer(async (req, res) => {
-    const __dirname = path.dirname(new URL(import.meta.url).pathname);
-    const absolutepath= path.join(__dirname,'index.html')
-   
-    try{
-      const content = await fs.readFile(absolutepath, 'utf8');
-    }catch(errr){
-      console.log(errr)
-    }
+    try {
+        const absolutePath = path.join(__dirname, "index.html");
 
-    console.log(absolutepath)
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-  // res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end(htmlContent);
+        console.log(absolutePath);
+
+        const content = await fs.readFile(absolutePath, "utf8");
+
+        res.writeHead(200, {
+            "Content-Type": "text/html",
+        });
+
+        res.end(content);
+    } catch (err) {
+        console.error(err);
+
+        res.writeHead(500, {
+            "Content-Type": "text/plain",
+        });
+
+        res.end("Internal Server Error");
+    }
 });
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
 // console.log(import.meta.url); process.CWD for current working directory (import.meta.dirname0)
